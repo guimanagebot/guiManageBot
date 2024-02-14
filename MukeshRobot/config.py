9019db3fa2099adba99d898b350c2dec1ca90889
ataxion <agustinus@axion.ltd>
2024-02-14 17:51:00 +0700

    add config

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -9,19 +9,19 @@ class Config(object):
     TOKEN = "6548824436:AAF5QtjsNqiv9OwF6EHotluNmp8XfkMa7Us"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 2145093972 # If you dont know, run the bot and do /id in your private chat with it, also an integer
     
-    SUPPORT_CHAT = "the_support_chat"  # Your own group for support, do not add the @
-    START_IMG = ""
+    SUPPORT_CHAT = "Support_GUI"  # Your own group for support, do not add the @
+    START_IMG = "https://id.pinterest.com/pin/520306563204995674/"
     EVENT_LOGS = ()  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
-    MONGO_DB_URI= "103.125.43.53/32"
+    MONGO_DB_URI= "mongodb+srv://guimanage14:<BnYCaTEcTnkWxEVQ>@cluster0.ylagj6l.mongodb.net/?retryWrites=true&w=majority"
     # RECOMMENDED
-    DATABASE_URL = ""  # A sql database url from elephantsql.com
+    DATABASE_URL = "postgres://wpmbbjfm:QDiX48aMyWOz0GIUtYzq96IMRiSv3YyC@rain.db.elephantsql.com/wpmbbjfm"  # A sql database url from elephantsql.com
     CASH_API_KEY = (
-        ""  # Get your API key from https://www.alphavantage.co/support/#api-key
+        "4SR9UQ224AON4294"  # Get your API key from https://www.alphavantage.co/support/#api-key
     )
-    TIME_API_KEY = ""
+    TIME_API_KEY = "PEV3WLHPGI2V"
     
     # Get your API key from https://timezonedb.com/api
-    API_KEY=""
+    API_KEY="2006156742-mukesh-api-|"
     #generate api from telegram using /token command bot username :>> @adventure_robot
 
     # Optional fields
